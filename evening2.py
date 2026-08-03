04658a530c32cbfb485e31ea4442728040498d93
bhawana <bhawanahirnwal0@gmail.com>
2026-08-03 14:16:22 +0530

    loops program

--- evening2.py
+++ evening2.py
@@ -6,10 +6,86 @@
 #    idx=idx+1
 
 #que5
-digits = [1,4,9,16,25,36,49,64,81,100]
-x=25
-idx = 0
-while idx<len(digits):
-    if digits[idx] == x:
-     print("print found number:",idx)
-    idx=idx+1
+#digits = [1,4,9,16,25,36,49,64,81,100]
+#x=25
+#idx = 0
+#while idx<len(digits):
+#    if digits[idx] == x:
+ #    print("print found number:",idx)
+ #   idx=idx+1
+
+
+#list = [1,2,3,4,5,6,7]
+#for el in list:
+#    print(el)
+
+#tup = (1,2,3,4,5,6,7)
+#for el in tup:
+#    print(el)
+
+#list = [1,4,9,16,25,36,49,64,81,100]
+#for el in list:
+ #       print(list)
+        
+
+#tup = (1,4,9,16,25,36,49,64,81,100)
+#x=25
+#idx = 0
+#for el in tup:
+#      if(el==x):
+#        print("found:",x)
+#        break
+#      idx = idx+1
+
+#print(range(4))
+#seq = range(4)
+#for el in seq:
+#    print(el)
+
+
+#for i in range(3):#(stop)
+     #print(i)
+
+#for i in range(2 ,10):#(start,stop)
+#    print(i)
+
+#for i in range(2, 10 ,2):#(start,stop, step)
+#    print(i)
+
+#for i in range(2, 100, 2):
+ #   print(i)
+
+#for i in range(1, 100, 2):
+ #   print(i)
+
+#for i in range(101):
+ #   print(i)
+
+#for  i in range(100, 0,  -1):
+ #   print(i)
+
+#n = int(input("enter the number:"))
+#for i in range(1, 11):
+ #  print(n*i)       
+
+#n = 100
+#sum = 0
+#for i in range(1, n+1):
+#    sum += i
+#print("total sum =", sum)
+
+#
+#n = 5
+#fact =1
+#i = 1
+##while i<=n:
+ #   fact *= i
+   # i = i+1
+#print("factorial=",fact)
+n = 5
+fact = 1
+for i in range(1, n+1):
+    fact *=i
+print("factorial =",fact)
+    
+    
