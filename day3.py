e1f62d6c399e80543314442e7583344f33ca4aab
bhawana <bhawanahirnwal0@gmail.com>
2026-08-03 21:28:31 +0530

    functions  practice

---
+++ day3.py
@@ -0,0 +1,55 @@
+#def calc_sum(a,b):
+ #   sum = a+b
+ #   print(sum)
+#    return sum
+#calc_sum(5,2)
+#calc_sum(17,25)
+
+#def calc_sum(a,b):#parameters
+#    return a+b
+
+#sum=calc_sum(6,8)#function call;
+#print(sum)
+
+#def print_hello():
+ #   print("hello")
+
+#print("hello")
+
+# sum of avg of 3 numbers
+
+#def calc_avg(a,b,c):
+ #   sum = a+b+c
+ #   avg = sum/2
+  #   return avg
+#calc_avg(3,4,5)
+
+#print("bhawanhirnwal", end=" ")
+#print("shilpi")
+
+
+
+#digits = [2,5,6,8,9,5,7,3,1]
+#even = [2,4,6,8,10,12,14,16,18,20]
+
+#def print_len(list):
+  #  print(len(list))
+
+#print_len(digits)
+#print_len(even)
+
+#n =5
+
+#def calc_fact(n):
+ #   fact = 1
+ #  for i in range(1, n+1):
+  #      fact *= i
+  #  print(fact)
+
+#calc_fact(6)
+
+def convertor(usd_val):
+    inr_val = usd_val * 83
+    print(usd_val , "USD =", inr_val ,"INR")
+
+convertor(100)
