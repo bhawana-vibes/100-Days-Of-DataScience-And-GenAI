eb245bda1856b6fe4e7f6b595b627a52d45ba98f
bhawana <bhawanahirnwal0@gmail.com>
2026-08-03 12:08:35 +0530

    practice on loops

--- evening2.py
+++ evening2.py
@@ -1,4 +1,15 @@
-i = 1
-while i<=10:
-    print(6*i)
-    i=i+1
+# 
+#digits = [1,4,9,16,25,36,49,64,81,100]
+#idx = 0
+#while idx<len(digits):
+#    print(digits[idx])
+#    idx=idx+1
+
+#que5
+digits = [1,4,9,16,25,36,49,64,81,100]
+x=25
+idx = 0
+while idx<len(digits):
+    if digits[idx] == x:
+     print("print found number:",idx)
+    idx=idx+1
