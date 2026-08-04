d4d09f69af47fd6281f2223c8453520cadba8be6
bhawana <bhawanahirnwal0@gmail.com>
2026-08-04 15:40:12 +0530

    input and output files program

--- day4.py
+++ day4.py
@@ -1,3 +1,21 @@
-f = open("demo.txt", "a")
-f.write(" i am bhawana hirnwal")
-f.close()
+#f = open("demo.txt", "r+")
+#f.write("\n i am bhawana hirnwal")
+#print(f.read())
+#f.close()
+
+#with open("demo.txt","r") as f:
+ #  data = f.read()
+ #  print(data)
+
+#import os
+
+#with open("practice.txt","w") as f:
+#    f.write("Hi everyone\nwe are learning python\ntopic on file input and output")
+
+with open("practice.txt","r") as f:
+      data = f.read()
+
+new_data = data.replace("python","java")
+print(new_data)
+
+
